1c5486b075f9f787c63ed1d5bd70c6629af66408
kosta <kosta@kosta-PC>
2017-05-24 14:28:27 +0900

    지혜가 추가한 짜잔면

--- SampleProjectForGit/src/Test.java
+++ SampleProjectForGit/src/Test.java
@@ -5,5 +5,8 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("뚜비두밥 오뚜기밥");
 	}
-
+	public static void jihye(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("짜장짜장 짜잔 면");
+	}
 }

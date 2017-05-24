5ce4883ae7ec23b641105ce2e8558d90095a9dce
kosta <kosta@kosta-PC>
2017-05-24 14:23:50 +0900

    두 번째 커밋

--- SampleProjectForGit/src/Test.java
+++ SampleProjectForGit/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("뚜비두밥 오뚜기밥");
+		
+		System.out.println("하나 더 추가함 ^^");
 	}
 
 }
